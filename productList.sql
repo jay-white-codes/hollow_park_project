b2241428854b206e9a058e888a45051d67b1d148
Jay White <103801730+jay-white-codes@users.noreply.github.com>
2022-08-21 00:55:49 -0500

    Add files via upload

---
+++ productList.sql
@@ -0,0 +1,90 @@
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+   values ('49042129173', 'Mossimo Supply Co.', 'L', 'Mens Cargo Pants', null, '5', '24.99','30', '34', null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('019783343884', 'Haggar', 'XL', 'Mens Straight Fit Canvas Pants', null, '5', '44.99', '30', '40', null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('0512177305990', 'Levi', 'XL', 'Mens 550 Straight Fit Jeans', null, '5', '59.99', '30', '40', null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('084084708007', 'Wrangler', 'M','Mens Reuglar Fit Jeans', null, '5', '18.87','32', '34', null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('887035131103', 'Levi', 'S', 'Mens 502 Taper Fit Jeans', null, '5', '49.99', '32', '30', null); 
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('190416987733', 'Levi', 'S', 'Mens Carrier Cargo Shorts', null, '5', '49.50', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('613053383923', 'X-Ray', 'M', 'Mens Tactical Cargo Shorts', null, '5', '29.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('194498291054', 'Nike', 'L', 'Mens Basketball Shorts', null, '5', '18.81', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('193655166532', 'Nike', 'XL', 'Mens Dri-Fit Basketball Shorts', null, '5', '35.00', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('60764506853', 'Dickies', 'XL', 'Mens Loose Fit Pocket Work Shorts', null, '5', '29.99', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('607645073132', 'Dickies', 'L', 'Mens Short Sleeve Twill Shirt', null, '5', '24.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('011919748907', 'Hanes', 'XL', 'Mens Long Sleeve CoolDri Performance T-Shirt', null, '5', '10.00', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('029311849115', 'Dickies', 'M', 'Mens Heavyweight Crew Nect Short Sleeve T-Shirt', null, '5', '19.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('19250310467', 'Champion', 'S', 'Mens Script Logo Classic Jersey Graphic T-Shirt', null, '5', '25.00', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('194004478955', 'Columbia', 'M', 'PFG Terminal Tackle Heather Long Sleeve T-Shirt', null, '5', '40.00', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('826218005718', 'Nike', '10', 'Air Force 1 Mid', null, '5', '104.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('193153985291', 'Nike', '11', 'Reax 8 TR Cross-Training Shoes', null, '5', '79.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('885176767410', 'Nike', '9', 'Benassi JDI White Black Slides', null, '5', '25.00', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('029599000055', 'Gucci', '10', 'Jordaan Horesbit Gucci Loafers', null, '5', '920.00', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('194502170207', 'Jordan', '12', 'Nike Air XXXV DNA', null, '5', '303.74', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('672712330951', 'A New Day', '12', 'Womens Casual Denim Overall Romper - Black', null, '5', '48.30', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('490180424039', 'A New Day', '14', 'Womens Mid-Rise Regular Fit Pleated Pants', null, '5', '27.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('810713032553', 'Satva', '8', 'Womens Organic Cotton Mantra Yoga Pants', null, '5', '14.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('195339264114', 'Levi', '22', 'Womens Plus Size 721 High-Rise Skinny Jeans', null, '5', '48.65', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('008867963971', 'Sofia Vergara', '4', 'Womens Mid-Rise Skinny Ankle Jeans', null, '5', '22.5', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('884968323841', 'A New Day', 'S', 'Womens Athletic Biker Shorts', null, '5', '10.98', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('193239787009', 'Levi', 'M', 'Womens High-Rise Jean Shorts', null, '5', '21.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('008866921125', 'Sofia Vergara', 'L', 'Womens Mid-Rise Destructed Hem Shorts', null, '5', '25.50', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('191906835893', 'A New Day', 'XL', 'Womens Mid-Rise Tie Waist Shorts', null, '5', '19.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('192660949321', 'Columbia', 'M', 'Womens PFG Bonehead Stretch Shorts', null, '5', '50.00', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('193441571793', 'The Pioneer Woman', 'M', 'Womens Scoop Neck Sleeveless Top', null, '5', '12.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('490220719316', 'Cuddl Duds', 'M', 'Womens Everyday Comfort Thermal Crewneck Top', null, '5', '16.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('984113275114', 'Jedewomi', 'L', 'Womens Casual Blouse Button-Down Colorful Long Sleeve Top', null, '5', '31.50', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('743425079649', 'Lumento', 'S', 'Womens Comfy Gradient Summer Top', null, '5', '8.98', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('03335902687', 'Wendunide', 'XL', 'Womens Summer Printed Short Sleeve Top', null, '5', '23.80', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('699942920295', 'Boho', 'S', 'Womens Geometric Boho Batwing Sleeve Dress', null, '5', '33.98', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('192351347269', 'Calvin Klein', 'M', 'Womens Short Sleeved Seamed Sheath Causual Dress', null, '5', '89.98', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('086569323064', 'INK+IVY', 'S', 'Womens Maxi Dress', null, '5', '30.30', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('886783006558', 'Emmani', 'L', 'Womens Luxury Sequin Beaded Tulle Dress', null, '5', '129.99', null, null, null); 
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('707906871344', 'Boho', 'XL', 'Womens Summer Casual T-Shirt Dress', null, '5', '17.95', null, null, null);
+    insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('022859552639', 'Converse', '10', 'Womens Chuck Taylor All Star High Top Shoe', null, '5', '64.90', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('193605344300', 'Juicy Couture', '8', 'Womens Black Rubber Sandals', null, '5', '29.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('605388013309', 'Time and Tru', '7', 'Womens Bow Thong Sandal', null, '5', '14.98', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('400947825856', 'Jennifer Lopez', '11', 'Womens Hooded Platform High Heel Shoe', null, '5', '74.99', null, null, null);
+insert into Product (id, itemName, itemSize, description, image, inStock, listPrice, length, width, height)
+	values ('870220450158', 'Journee', '6', 'Womens Round Toe Canvas Mid-Calf Boot', null, '5', '80.00', null, null, null);
